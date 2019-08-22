b76fd37186577ba7018a8b42f7e5e4c8d3062620
Claus Hoffmann <claus@yeah.de>
2019-08-22 21:14:24 +0200

    searchbox styles

--- gulpfile.js/gulpdocs.js
+++ gulpfile.js/gulpdocs.js
@@ -1,11 +1,15 @@
 const gulp = require("gulp")
 var exec = require('child_process').exec;
 
-
 gulp.task("docs", (cb) => {
 	exec('doxygen Doxyfile', function (err, stdout, stderr) {
 		console.log(stdout);
 		console.log(stderr);
-		cb(err);
+		exec('cp ./public/css/search.css ./htdocs/search/', function (err, stdout, stderr) {
+			console.log(stdout);
+			console.log(stderr);
+			cb(err);
+		})
+		
 	  });
 })
